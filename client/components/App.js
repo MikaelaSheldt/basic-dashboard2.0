02a05382f2b4a7869a46be86cb1d5c0669543cae
Mikaela Sheldt <msheldt@gmail.com>
2019-06-20 18:26:22 -0400

    FEAT(client) connect inputfield component to redux store and index.html

---
+++ client/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import InputField from "./InputField"
+
+const App = () => {
+  return (
+    <div>
+      <h1> DASHBOARD </h1>
+      <InputField />
+    </div>
+  )
+}
+
+export default App
